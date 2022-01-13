144410cbf3635ec99c3c60dab97a543d9af6bfb6
iAnthonyCoder <anthonymedina958@gmail.com>
2022-01-12 20:18:04 -0400

    update

--- controllers/metadataController.js
+++ controllers/metadataController.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-AWS.config.update({accessKeyId:"AKIAYIXALYDSZQNZDT5M", secretAccessKey:"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"});
+AWS.config.update({accessKeyId:process.env.ACCESS_KEY_ID, secretAccessKey:process.env.SECRET_ACCESS_KEY});
 const s3 = new AWS.S3();
 
 
